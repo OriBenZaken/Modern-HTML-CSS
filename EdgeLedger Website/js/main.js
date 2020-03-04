f725daad6a205e5fc3340693f0017fd38431438f
ori ben <ori.ben.zaken@wdc.com>
2020-03-04 23:34:04 +0200

    commit

--- EdgeLedger Website/js/main.js
+++ EdgeLedger Website/js/main.js
@@ -1,3 +1,4 @@
+// Google Map
 var map;
 function initMap() {
   const loc = {lat: 32.085300, lng: 34.781769}
@@ -7,3 +8,28 @@ function initMap() {
   });
   const marker = new google.maps.Marker({ position:loc, map: map });
 }
+
+// Sticky menu background 
+window.addEventListener('scroll', ()=> {
+  if (window.scrollY > 100) {
+    document.getElementById('navbar').style.opacity = 0.9;
+  } else {
+    document.getElementById('navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+      scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
